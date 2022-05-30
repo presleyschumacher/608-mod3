aaec447578aaf5fe0092f5ac66b14f7f3933aa26
Presley <105391626+presleyschumacher@users.noreply.github.com>
2022-05-30 11:23:06 -0500

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,1 +1,39 @@
 
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import math
+
+
+# In[2]:
+
+
+math.sqrt(900)
+
+
+# In[3]:
+
+
+math.floor(9.2)
+
+
+# In[4]:
+
+
+math.fabs(-10)
+
+
+# In[5]:
+
+
+pow(2.0, 7.0)
+
+
+# In[ ]:
+
+
+
+
